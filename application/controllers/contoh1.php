cbc854351c233c39d46a07b9edaa0aa5392bd5ab
azizdzu98 <azizdzulkarnain2105@gmail.com>
2024-03-23 14:10:34 +0700

    Tugas COntoh 1, Contoh 2 dan Contoh 3

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class contoh1 extends CI_Controller
+{ 
+    public function index ()
+    { 
+        echo "<h1>PERKENALKAN</h1>";
+        echo "Nama saya AZIZ DZULKARNAEN
+              Saya tinggal di Bekasi Utara, masih di BUMI.";
+    }
+}
